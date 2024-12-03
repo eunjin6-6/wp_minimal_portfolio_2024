c2cbfdde5de956dccee5f27d3aac06392a360ccb
eunjin6-6 <dmswls616@gmail.com>
2024-12-03 13:18:04 +0900

    first commit

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minimal Portfolio</title>
+    <?php wp_head(); ?>
+    <!-- 
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/common.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/default.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/responsive.css">
+     -->
+</head>
+<body>
+    <header>
+       <h1 class="logo"><a href="">Minimal Portfolio Theme</a></h1>
+       <nav>
+           <ul>
+               <li><a href="index.html">Home</a></li>
+               <li><a href="portfolio.html">Portfolio</a></li>
+               <li><a href="about.html">About</a></li>
+               <li><a href="contact.html">Contact</a></li>
+           </ul>
+       </nav>              
+    </header>
+    <hr>
+    <main class="content">
