f405bef902fd16824d1093528ac027787d41d174
eunjin6-6 <dmswls616@gmail.com>
2024-12-05 10:26:08 +0900

    메뉴 생성, 최근글 출력

---
+++ front-page.php
@@ -0,0 +1,62 @@
+<?php get_header() ?>
+
+<div class="container latest_portfolio">
+  <div class="row intro">
+      <div class="col-md-4">
+          <div class="contents shadow">
+              <h2 class="heading2">I'm alikerock</h2>
+          </div>
+      </div>
+      <div class="col-md-4">
+          <div class="contents shadow">
+              <h2 class="heading2">I create super awesome stuff</h2>
+          </div>
+      </div>
+      <div class="col-md-4">
+          <div class="contents shadow">
+              <h2 class="heading2">I'm available for freelance projects</h2>
+          </div>
+      </div>
+  </div>
+  <div class="row list">
+  <?php
+    query_posts( array(
+      'category_name'  => 'minimal-portfolio',
+      'posts_per_page' => 3
+    ) );
+
+    // The Loop
+    while ( have_posts() ) : the_post();
+    ?>
+
+    <div class="col-md-4">
+        <div class="contents shadow">
+            <?php
+              if ( has_post_thumbnail() ) {
+                  the_post_thumbnail( 'full' );
+              }
+            ?>
+            <div class="hover_contents">
+                <div class="list_info">
+                    <h3>
+                      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
+                      <img src="<?= IMAGES ?>/portfolio_list_arrow.png" alt="list arrow">
+                    </h3>
+                    <p><a href="<?php the_permalink(); ?>">Click to see project</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  <?php 
+    endwhile;
+  ?>
+
+      
+  </div>
+  <p class="porfolio_readmore">
+      <a href="" class="primary-btn">See my full portfolio</a>        
+  </p>
+</div>
+
+<?php get_footer() ?>
--- functions.php
+++ functions.php
@@ -1,25 +1,34 @@
+
 <?php
 
 /* 변수 등록 */
+define('THEMEROOT',get_stylesheet_directory_uri());
+define('IMAGES',THEMEROOT.'/images');
 
-define('THEMEROOT', get_stylesheet_directory_uri());
-define('IMAGES', THEMEROOT.'/images');
-
-/* 함수 생성 */
 if(! function_exists('minimal_script')){
   function minimal_script(){
-
     //script 등록
-    wp_register_script( 'bootstrap-js', THEMEROOT.'/js/bootstrap.min.js', true, false, true );
+    wp_register_script('bootstrap-js',THEMEROOT.'/js/bootstrap.min.js', true,false,true);
+    
     //script 로드
-    wp_enqueue_script( 'bootstrap-js' );
+    wp_enqueue_script('bootstrap-js');
 
     //css 로드
-    wp_enqueue_style( 'common-css', THEMEROOT.'/css/common.css' );
-    wp_enqueue_style( 'bootstrap-gired', THEMEROOT.'/css/bootstrap-grid.min.css' );
-    wp_enqueue_style( 'default', THEMEROOT.'/css/default.css' );
-    wp_enqueue_style( 'responsive', THEMEROOT.'/css/responsive.css' );
-
+    wp_enqueue_style('common-css',THEMEROOT.'/css/common.css');
+    wp_enqueue_style('bootstrap-grid',THEMEROOT.'/css/bootstrap-grid.min.css');
+    wp_enqueue_style('default',THEMEROOT.'/css/default.css');
+    wp_enqueue_style('responsive',THEMEROOT.'/css/responsive.css');
   }
-  add_action ( 'wp_enqueue_scripts', 'minimal_script');
+  add_action('wp_enqueue_scripts', 'minimal_script');
 }
+
+function register_my_menu() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' )
+     )
+   );
+ }
+ add_action( 'init', 'register_my_menu' );
+
+add_theme_support( 'post-thumbnails' );
