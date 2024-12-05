173624b9dfe40a430ee0b4f19c3e55dec76b5d6a
eunjin6-6 <dmswls616@gmail.com>
2024-12-05 13:21:08 +0900

    contact.php 생성

---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php 
+/* Template Name: Contact Template */
+?>
+<?php get_header() ?>
+
+
+  <div class="container">
+    <h2>contact form</h2>
+    <?php the_content(); ?>
+  </div>
+<?php get_footer() ?>
