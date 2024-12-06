2870e599e86be88d825708a012c6d5a001909919
eunjin6-6 <dmswls616@gmail.com>
2024-12-06 10:21:18 +0900

    contact form 추가

--- contact.php
+++ contact.php
@@ -1,11 +1,41 @@
 <?php 
 /* Template Name: Contact Template */
 ?>
+
 <?php get_header() ?>
+
+<div class="container about_content shadow">
+    <div class="contact">
+        <h3 class="heading6">Let’s Get in Touch</h3>
+        <p>
+        You can call me, email me directly or connect with me through my social networks.
+        </p>
+        <p>
+            (+40) 744122222<br/>
+            <a href="mailto:hello@adipurdila.com">hello@adipurdila.com</a>               
+        </p>
+        <ul class="social_links">
+            <li><a href=""><img src="<?= IMAGES; ?>/twitter.png" alt="twitter"></a></li>
+            <li><a href=""><img src="<?= IMAGES; ?>/facebook.png" alt="facebook"></a></li>
+            <li><a href=""><img src="<?= IMAGES; ?>/dribble.png" alt="dribble"></a></li>
+        </ul>                                
+    </div>
+    <hr class="double">
+    <div class="form">
+        <h3 class="heading6">Need a Quote?</h3>
+        <p>
+            Use the form below. All fields are required.
+        </p>
+        <div class="contact_form">
+          <?php echo do_shortcode('[contact-form-7 id="a18fc27" title="My contact form"]'); ?>
+        </div>
+    </div>
+</div>
 
 
   <div class="container">
     <h2>contact form</h2>
     <?php the_content(); ?>
+    
   </div>
 <?php get_footer() ?>
